a61b2f71e596f91dc214dd2f1309d69a233a9a91
伊東和成 <itokazunari5375@icloud.com>
2022-04-03 08:41:21 +0900

    レンダリングをテスト

--- src/Render.test.tsx
+++ src/Render.test.tsx
@@ -2,9 +2,21 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Render } from "./Render";
 
+// describe("Rendering", () => {
+//   it("Should render all elements collect", () => {
+//     render(<Render />);
+//     screen.debug(screen.getByRole("heading"));
+//   });
+// });
+
 describe("Rendering", () => {
-  it("Should render all elements collect", () => {
+  it("should render all elements collect", () => {
     render(<Render />);
-    screen.debug(screen.getByRole("heading"));
+    expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+    screen.debug(screen.getByText("Test"));
+    expect(screen.getByTestId("copyright")).toBeTruthy();
   });
 });
